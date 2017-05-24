b1f1bf6da2510d9ec6bdcbfd5bf3c205147a8578
RAJA <yvahn.raja@gmail.com>
2017-05-24 09:23:19 +0200

    push de test insertion liste

---
+++ web/page.js
@@ -0,0 +1,63 @@
+
+
+function modifier() {
+    viderBouton("B2Cell");
+    afficherB1("valider");
+
+}
+
+
+function  valider() {
+    afficherB1("modifier");
+
+    supprimer();
+}
+
+function supprimer() {
+    viderBouton("B1Cell");
+    afficherB2("confirmer");
+
+}
+
+function confirmerSuppression() {
+
+    afficherB2("supprimer")
+    modifier();
+
+}
+
+function viderBouton(val) {
+    document.getElementById(val).innerHTML =
+            "";
+
+}
+
+function afficherB1(val) {
+    switch (val) {
+        case "modifier":
+            var txt = "<button onclick=\"modifier()\" "
+                    + "value=\"B1\">Modifier<\/button>";
+            break;
+
+        case "valider":
+            var txt = "<button onclick=\"valider()\" value=\"B1\">Valider<\/button>";
+            break;
+    }
+
+
+    document.getElementById("B1Cell").innerHTML = txt;
+
+}
+function afficherB2(val) {
+    switch (val) {
+        case "supprimer":
+            var txt = "<button onclick=\"confirmerSuppression()\" "
+                    + "value=\"B2\">Modifier<\/button>";
+            break;
+
+        case "confirmer":
+            var txt = "<button onclick=\"supprimer()\" value=\"B2\">Valider<\/button>";
+            break;
+    }
+    document.getElementById("B2Cell").innerHTML = txt;
+}
